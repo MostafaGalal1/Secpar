6c47fce1e55fd2b1d81de4ef4a87cd7b355282a7
MahmoudGoda2003 <95986689+MahmoudGoda2003@users.noreply.github.com>
2023-10-10 00:17:43 +0300

    finished without adding galal

--- setup.py
+++ setup.py
@@ -1,8 +1,34 @@
 from setuptools import setup, find_packages
+import codecs
+import os
 
+# Get the current directory
+here = os.path.abspath(os.path.dirname(__file__))
 
+# Read the README file as long description
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = fh.read()
+
+VERSION = '1.0.0'
+DESCRIPTION = 'Scrape problems submissions from different platforms'
+
+# Define the authors
+author_name_1 = 'Mahmoud Goda'
+author_email_1 = 'mahmoden17@gmail.com'
+author_name_2 = 'Your Co-Author'
+author_email_2 = 'coauthor@example.com'
+
+# Combine author information
+authors = f"{author_name_1} <{author_email_1}>, {author_name_2} <{author_email_2}>"
+
+# Setting up
 setup(
     name="Secpar",
+    version=VERSION,
+    author=authors,
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
     packages=find_packages(),
     include_package_data=True,
     entry_points={
@@ -13,8 +39,16 @@ setup(
         "PyGithub>=1.54.0",
         "requests>=2.0.0",
         "beautifulsoup4>=4.0.0",
-        "argparse>=1.4.0",
         "stem>=1.8.0",
         "PySocks>=1.7.1,<2.0.0",
+        "argparse>=1.4.0",
+    ],
+    keywords=['CP', 'scraper', 'secpar', 'problem solving', 'python', 'tools'],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
     ]
 )
